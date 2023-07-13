6eab2fe4601c7d8b39e191d85d330430bbd50cc1
MicDalangin51 <martindalangin@gmail.com>
2023-07-13 13:33:38 +0800

    feat: create mechanism for registering a new user

--- src/index.js
+++ src/index.js
@@ -1,28 +1,11 @@
-console.log("hello")
-import { build } from './app.js';
-import Fastify from 'fastify';
-import { general } from './services/general/index.js';
+import { general } from './general/index.js';
+import { registerUser } from './users/register-user.js';
 
-const prefix = '/api';
-async function start () {
-  try {
-    
-    const fastify = await build();
-    
-
-    const addr = await fastify.listen({
-      port: '8080'
-    });
-    
-
-    console.log(`Listening on ${addr}`);
-    
-  } catch (error) {
-    // prints the error
-    console.error(error);
-    // exits the program with an error code
-    process.exit(1);
+export class Service {
+  constructor (app) {
+    this.app = app;
   }
-}
 
-start();
+    general=general
+    registerUser=registerUser
+}
