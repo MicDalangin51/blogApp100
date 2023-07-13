b83a564e5065765327af1f3f84f72ec5787d9f1d
MicDalangin51 <martindalangin@gmail.com>
2023-07-13 13:58:01 +0800

    feat: setup the swagger API sepcification for register user

--- src/specification/index.js
+++ src/specification/index.js
@@ -1,18 +1,18 @@
 import { paths } from './paths/index.js';
-// import { components } from './components/index.js';
+import { components } from './components/index.js';
 
 export const specification = {
   openapi: '3.0.0',
   info: {
-    title: 'Blog App',
+    title: 'My Blog Application',
     version: '0.0.1'
   },
 
   paths,
-  // components,
+  components,
   security: [
     {
-      // cookieAuth: []
+      cookieAuth: []
     }
   ]
 };
--- src/specification/paths/index.js
+++ src/specification/paths/index.js
@@ -1,7 +1,7 @@
 import { general } from './general/index.js';
-
+import { user } from './user/index.js';
 
 export const paths = {
   ...general,
-
+  ...user
 };
