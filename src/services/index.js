dc30d3143bbd51adc65e7e17bf2c5e99c75e9072
MicDalangin51 <martindalangin@gmail.com>
2023-07-13 13:41:51 +0800

    feat: add a service for registering a new user

--- src/services/index.js
+++ src/services/index.js
@@ -1,11 +1,11 @@
-
-import { general } from "./general/index.js";
+import { general } from './general/index.js';
+import { registerUser } from './users/register-user.js';
 
 export class Service {
   constructor (app) {
     this.app = app;
   }
 
-    general=general
-
+    general = general
+    registerUser = registerUser
 }
