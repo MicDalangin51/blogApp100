11e9e140d2b9eb115b37300b085cc21ea0929c37
MicDalangin51 <martindalangin@gmail.com>
2023-07-18 15:19:07 +0800

    feat: add the change own password page along with path

--- src/frontend/routes.js
+++ src/frontend/routes.js
@@ -42,26 +42,16 @@ export const routes = {
       () => import('./pages/page-register/index.js')
     ]
   },
-//   '/todos': {
-//     render: () => html`
-//       <page-todos></page-todos>
-//     `,
-//     // runs all scripts, if any one of the functions in the list fails
-//     preRender: [
-//       redirectIfLoggedOut,
-//       () => import('./pages/page-todos/index.js')
-//     ]
-//   },
-//   '/todos/:id': {
-//     render: () => html`
-//       <page-todo-one .paramObject=${router.paramObject}></page-todo-one>
-//     `,
-//     // runs all scripts, if any one of the functions in the list fails
-//     preRender: [
-//       redirectIfLoggedOut,
-//       () => import('./pages/page-todo-one/index.js')
-//     ]
-//   },
+  '/change-password': {
+    render: () => html`
+      <page-change-password></page-change-password>
+    `,
+    // runs all scripts, if any one of the functions in the list fails
+    preRender: [
+      redirectIfLoggedIn,
+      () => import('./pages/page-change-password/index.js')
+    ]
+  },
   '/logout': {
     render: () => html`
       <page-logout></page-logout>
