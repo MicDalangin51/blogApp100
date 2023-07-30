5f528a94635931e8f479398ea79dace7382ec782
MicDalangin51 <martindalangin@gmail.com>
2023-07-30 12:29:09 +0800

    fix: the change user data page should not return to the blog list page upon a successful response

--- src/frontend/components/user-data-components/change-user-data-component/index.js
+++ src/frontend/components/user-data-components/change-user-data-component/index.js
@@ -38,7 +38,6 @@ class Component extends LitNoShadow {
     });
     if (response.status === 200) {
       this.errorMessage = '';
-      return changeUrl('/blog');
     }
     const { message, error } = await response.json();
     this.errorMessage = `HTTP Code: ${response.status} - ${error} - ${message}`;
